2ce72ad290e24542db56e3cc1a3150b95f411780
avemarcus <mark.kachanovskiy@gmail.com>
2024-10-06 13:00:35 +0300

    head script upd

--- bio_seq.py
+++ bio_seq.py
@@ -1,3 +1,14 @@
+"""
+
+The bio_seq package is designed to facilitate bioinformatics operations on DNA and RNA sequences. 
+It contains tools for filtering fastq files and performing sequence operations.
+
+run_dna_rna_tools - takes nucleotide sequences and performs various operations on them.
+
+filter_fastq - filters fastq sequences based on GC content, length, average quality.
+
+"""
+
 from .scripts_stock.run_dna_rna_tools import run_dna_rna_tools
 
 from .scripts_stock.filter_fastq import filter_fastq
